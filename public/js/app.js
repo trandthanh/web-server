2f7a6ea14a6791f55b77e4c00fef6517eec15598
Dang-Thanh Tran <trandgthanh@gmail.com>
2019-05-30 17:43:48 +0200

    changed interpolation to concatenation

--- public/js/app.js
+++ public/js/app.js
@@ -16,7 +16,7 @@ weatherForm.addEventListener('submit', (e) => {
   messageOne.textContent = 'Loading...';
   messageTwo.textContent = '';
 
-  fetch(`/weather?address=${location}`)
+  fetch('/weather?address=' + location)
     .then((response) => {
       response.json().then((data) => {
         if (data.error) {
